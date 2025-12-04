65491bf2233489c7beb4cb0d751070ee23cc06fc
thefreezecroc <goodplayer662@gmail.com>
2025-12-04 10:49:52 +0000

    added the camera switch button, it still has no functionallity, fixed some other UI / camera issues

--- lib/models/tile.dart
+++ lib/models/tile.dart
@@ -55,6 +55,7 @@ class _TileState extends State<Tile> {
     }
   }
 
+  /*
   CameraController? webController;
 
   Future<void> webCamera() async {
@@ -133,6 +134,7 @@ class _TileState extends State<Tile> {
                           ),
                         ),
                       ),
+                      
                     ),
                     )
                   ],
@@ -148,6 +150,163 @@ class _TileState extends State<Tile> {
     } catch (e) {
       print("Camera error: $e");
     } finally {
+      await webController?.dispose();
+      webController = null;
+    }
+  }
+  
+  */
+
+  CameraController? webController;
+
+  Future<void> webCamera() async {
+    _cameras = await availableCameras();
+    webController = CameraController(_cameras[0], ResolutionPreset.max);
+
+    try {
+      await webController!.initialize();
+      if (!mounted) return;
+
+      // Show live preview in a dialog
+      await showDialog(
+          context: context,
+          builder: (context) => Animate(
+                // effects: [FadeEffect(), ScaleEffect()],
+                child: AlertDialog(
+                  backgroundColor: Colors.black.withValues(alpha: 0.95),
+                  contentPadding: const EdgeInsets.fromLTRB(
+                      16, 16, 16, 16), // left, top, right, bottom
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(24),
+                    side: const BorderSide(color: Colors.white, width: 3),
+                  ),
+                  content: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: AspectRatio(
+                        aspectRatio: webController!.value.aspectRatio,
+                        child: CameraPreview(webController!),
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                           // ------------ CYCLE BUTTON -------------
+                        Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 6,
+                                  offset: Offset(0, 3)),
+                            ],
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(14),
+                              onTap: () async {
+                                // Capture image
+                                final picture =
+                                    await webController!.takePicture();
+                                webImage = await picture.readAsBytes();
+
+                                setState(() {
+                                  widget.imageTaken = true;
+                                });
+                                Navigator.pop(context); // Close dialog
+                              },
+                              child: Ink(
+                                height: 60, // button height
+                                width: 60, // button width
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(
+                                      14), // button corners
+                                  color: const Color.fromRGBO(255, 255, 255,
+                                      0.2), // semi-transparent background
+                                  border: Border.all(
+                                      color: Colors.white,
+                                      width: 2), // white border
+                                ), // End BoxDecoration
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.cameraswitch_outlined,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ), // End Icon
+                                ), // End inner Center
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 6,
+                                  offset: Offset(0, 3)),
+                            ],
+                          ),
+                          // ------------ SHUTTER BUTTON -------------
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(14),
+                              onTap: () async {
+                                // Capture image
+                                final picture =
+                                    await webController!.takePicture();
+                                webImage = await picture.readAsBytes();
+
+                                setState(() {
+                                  widget.imageTaken = true;
+                                });
+                                Navigator.pop(context); // Close dialog
+                              },
+                              child: Ink(
+                                height: 60, // button height
+                                width: 60, // button width
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(
+                                      14), // button corners
+                                  color: const Color.fromRGBO(255, 255, 255,
+                                      0.2), // semi-transparent background
+                                  border: Border.all(
+                                      color: Colors.white,
+                                      width: 2), // white border
+                                ), // End BoxDecoration
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ), // End Icon
+                                ), // End inner Center
+                              ),
+                            ),
+                          ),
+                        ),
+                      ], // children
+                    )
+                  ],
+                ),
+              )
+                  .animate()
+                  .slideY(
+                      begin: -1,
+                      end: 0,
+                      curve: Curves.easeOut,
+                      duration: 500.ms)
+                  .fadeIn(duration: 500.ms));
+    } catch (e) {
+      print("Camera error: $e");
+    } finally {
+      await Future.delayed(200.ms); // wait needed to properly dispose of the camera
       await webController?.dispose();
       webController = null;
     }
@@ -197,48 +356,45 @@ class _TileState extends State<Tile> {
                 :
                 //not taken the img
 
-
-                  Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: makeImage,
-                        splashColor: const Color.fromARGB(50, 255, 255, 255),
-                        child: Ink(
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        widget.task,
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          fontSize: screenWidth / (oszto * 8),
-                                          color: Colors.black,
-                                        ),
-                                        softWrap: true,
-                                      ),
-                                      Container(
-                                          height: size / 3,
-                                          width: size / 2,
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(10),
-                                          ),
-                                          child: Icon(
-                                            Icons.camera_alt_outlined,
-                                            size: size / 5,
-                                            color: const Color.fromRGBO(
-                                                255, 255, 255, 0.6),
-                                            weight: 1,
-                                          )),
-                                    ],
-
+                Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: makeImage,
+                      splashColor: const Color.fromARGB(50, 255, 255, 255),
+                      child: Ink(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                widget.task,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: screenWidth / (oszto * 8),
+                                  color: Colors.black,
+                                ),
+                                softWrap: true,
+                              ),
+                              Container(
+                                  height: size / 3,
+                                  width: size / 2,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Icon(
+                                    Icons.camera_alt_outlined,
+                                    size: size / 5,
+                                    color: const Color.fromRGBO(
+                                        255, 255, 255, 0.6),
+                                    weight: 1,
+                                  )),
+                            ],
                           ),
                         ),
                       ),
                     ),
-                )
+                  )
 
             /*
               Padding(
